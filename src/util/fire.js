e4044ac0c7da4503dfb7ca4f5de9a4a50c11d399
shuckdub <gabriel.aggernielsen@gmail.com>
2020-02-21 13:34:13 +0100

    database adds time, current and list of excerise sites, blocked and unblocked sites

---
+++ src/util/fire.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase';
+import "firebase/auth";
+import "firebase/firestore";
+
+import { API_KEY, APP_ID} from "./fireKeys";
+
+const firebaseConfig = {
+    apiKey: API_KEY,
+    authDomain: "the-distraction-shield.firebaseapp.com",
+    databaseURL: "https://the-distraction-shield.firebaseio.com",
+    projectId: "the-distraction-shield",
+    storageBucket: "the-distraction-shield.appspot.com",
+    messagingSenderId: "700316556439",
+    appId: APP_ID,
+    measurementId: "G-PZNS8G9787"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const provider = new firebase.auth.GoogleAuthProvider();
+
+export const auth = firebase.auth();
+
+export default firebase;
